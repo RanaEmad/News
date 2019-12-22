059759306c052f0c660749e8101b5e9a7b295648
Rana Emad <rana3emad@gmail.com>
2019-12-22 18:23:18 +0200

    add news class

---
+++ app/src/main/java/com/example/android/news/News.java
@@ -0,0 +1,16 @@
+package com.example.android.news;
+
+public class News {
+    private String title;
+    private String section;
+    private String author;
+    private String datePublished;
+
+     public News(String title, String section,String author,String datePublished){
+         this.title=title;
+         this.section=section;
+         this.author=author;
+         this.datePublished=datePublished;
+     }
+
+}
