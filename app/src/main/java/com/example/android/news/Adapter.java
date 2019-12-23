188fd4334dbf2588f67342f0d920183ed3859573
Rana Emad <rana3emad@gmail.com>
2019-12-23 09:15:51 +0200

    add adapter

---
+++ app/src/main/java/com/example/android/news/Adapter.java
@@ -0,0 +1,42 @@
+package com.example.android.news;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class Adapter extends ArrayAdapter<News> {
+
+    public Adapter(Context context, ArrayList<News> news){
+        super(context,0,news);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView==null){
+            convertView= LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent,false);
+        }
+        News currentNews= getItem(position);
+        TextView titleView= convertView.findViewById(R.id.title);
+        titleView.setText(currentNews.getTitle());
+
+        TextView sectionView= convertView.findViewById(R.id.section);
+        sectionView.setText(currentNews.getSection());
+
+        TextView authorView= convertView.findViewById(R.id.author);
+        authorView.setText(currentNews.getAuthor());
+
+        TextView datePublishedView= convertView.findViewById(R.id.datePublished);
+        datePublishedView.setText(currentNews.getDatePublished());
+
+        return convertView;
+    }
+}
