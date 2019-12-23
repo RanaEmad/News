9c036297a61efe9d52664407d13881c01dc57329
Rana Emad <rana3emad@gmail.com>
2019-12-23 11:49:15 +0200

    add query utils

---
+++ app/src/main/java/com/example/android/news/QueryUtils.java
@@ -0,0 +1,162 @@
+package com.example.android.news;
+
+
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Helper methods related to requesting and receiving news from The Guardian.
+ */
+public final class QueryUtils {
+
+    private QueryUtils() {
+    }
+
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+
+        return output.toString();
+    }
+
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+
+    /**
+     * Query the USGS dataset and return a list of {@link News} objects.
+     */
+    public static List<News> fetchNewsData(String requestUrl) {
+
+        URL url = createUrl(requestUrl);
+
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<News> news = extractResultsFromJson(jsonResponse);
+
+        return news;
+    }
+
+
+    /**
+     * Return a list of {@link News} objects that has been built up from
+     * parsing the given JSON response.
+     */
+    private static List<News> extractResultsFromJson(String newsJSON) {
+        if (TextUtils.isEmpty(newsJSON)) {
+            return null;
+        }
+
+        List<News> news = new ArrayList<>();
+
+        try {
+
+            JSONObject baseJsonResponse = new JSONObject(newsJSON);
+            JSONObject responseObject = baseJsonResponse.getJSONObject("response");
+
+
+            JSONArray resultsArray = responseObject.getJSONArray("results");
+
+            for (int i = 0; i < resultsArray.length(); i++) {
+
+                JSONObject currentNews = resultsArray.getJSONObject(i);
+
+//                JSONArray tagsArray = currentNews.getJSONArray("tags");
+//                JSONObject currentTagsObject = tagsArray.getJSONObject(0);
+                News newsObj = new News(currentNews.getString("webTitle"), currentNews.getString("sectionName"), currentNews.getString("author"), currentNews.getString("webPublicationDate"), currentNews.getString("webUrl"));
+
+                news.add(newsObj);
+            }
+
+        } catch (JSONException e) {
+           e.printStackTrace();
+        }
+        return news;
+    }
+}
