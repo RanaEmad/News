c8f46dce0655506450537648e2c830b8807b2dd2
Rana Emad <rana3emad@gmail.com>
2019-12-23 09:15:29 +0200

    edit news class

--- app/src/main/java/com/example/android/news/News.java
+++ app/src/main/java/com/example/android/news/News.java
@@ -5,12 +5,33 @@ public class News {
     private String section;
     private String author;
     private String datePublished;
+    private String url;
 
-     public News(String title, String section,String author,String datePublished){
+     public News(String title, String section,String author,String datePublished,String url){
          this.title=title;
          this.section=section;
          this.author=author;
          this.datePublished=datePublished;
+         this.url=url;
      }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDatePublished() {
+        return datePublished;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
