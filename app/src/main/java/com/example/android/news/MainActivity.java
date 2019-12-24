f86b4e3a6d0153c3637d39fc9d8fd37c4bfe4750
Rana Emad <rana3emad@gmail.com>
2019-12-24 14:40:53 +0200

    edit main activity

--- app/src/main/java/com/example/android/news/MainActivity.java
+++ app/src/main/java/com/example/android/news/MainActivity.java
@@ -3,37 +3,39 @@ package com.example.android.news;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Lifecycle;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
+import androidx.preference.PreferenceManager;
 
-import android.app.usage.UsageEvents;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.service.autofill.FillEventHistory;
-import android.util.EventLog;
-import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
 
-    private static final String REQUEST_URL ="https://content.guardianapis.com/search?api-key=test";
+    private static final String REQUEST_URL ="https://content.guardianapis.com/search";
 
     private Adapter adapter;
 
     private TextView emptyView;
+
+    private static final int NEWS_LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,55 +44,102 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         ListView listView= findViewById(R.id.list);
 
-        this.emptyView= findViewById(R.id.empty_view);
+        emptyView= findViewById(R.id.empty_view);
 
-        listView.setEmptyView(this.emptyView);
+        listView.setEmptyView(emptyView);
 
-        this.adapter= new Adapter(this, new ArrayList<News>());
+        adapter= new Adapter(this, new ArrayList<News>());
 
-        listView.setAdapter(this.adapter);
+        listView.setAdapter(adapter);
 
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getSupportLoaderManager();
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            emptyView.setText(R.string.no_internet);
+        }
+
+        //open link in browser
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                News currentNews = adapter.getItem(position);
+                Uri newsUri = Uri.parse(currentNews.getUrl());
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+                startActivity(websiteIntent);
+            }
+        });
 
     }
 
     @Override
-    public void onLoaderReset(@NonNull Loader<List<News>> loader) {
-
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<List<News>> loader, List<News> data) {
-
+    public void onLoaderReset(@NonNull Loader loader) {
+        adapter.clear();
     }
 
     @NonNull
     @Override
     public Loader<List<News>> onCreateLoader(int id, @Nullable Bundle args) {
-        return null;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String keyword = sharedPrefs.getString(
+                getString(R.string.settings_keyword_key),
+                getString(R.string.settings_keyword_default));
+
+        Uri baseUri = Uri.parse(REQUEST_URL);
+
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("api-key", "test");
+        uriBuilder.appendQueryParameter("format", "json");
+        uriBuilder.appendQueryParameter("q", keyword);
+        uriBuilder.appendQueryParameter("page", "1");
+        uriBuilder.appendQueryParameter("page-size", "20");
+        uriBuilder.appendQueryParameter("order-by", "newest");
+        return new NewsLoader(this,uriBuilder.toString());
     }
 
-    private class NewsAsyncTask extends AsyncTask<String,Void,List<News>>{
-        @Override
-        protected List<News> doInBackground(String... urls){
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<News>> loader, List<News> data) {
+        ConnectivityManager mngr=(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = mngr.getActiveNetworkInfo();
 
-            List<News> result = QueryUtils.fetchNewsData(urls[0]);
-            return result;
+        if(networkInfo !=null && networkInfo.isConnected()){
+            emptyView.setText(R.string.empty_view);
+            emptyView.setGravity(Gravity.CENTER);
+        }
+        else{
+            emptyView.setText(R.string.no_internet);
         }
 
-        @Override
-        protected void onPostExecute(List<News> data){
-            adapter.clear();
-            if(data !=null && !data.isEmpty()){
-                adapter.addAll(data);
-            }
+        adapter.clear();
+
+        if(data !=null && !data.isEmpty()){
+            adapter.addAll(data);
         }
+    }
 
-    }//end NewsAsyncTask
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
